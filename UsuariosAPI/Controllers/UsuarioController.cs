02023cc2b3c1af2a9aa9c8fd2f311c3f7559b61e
Marcela Brandao <marcela.brandao@mira.eti.br>
2024-01-09 20:53:30 -0300

    Adicionar arquivos de projeto.

---
+++ UsuariosAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Models;
+using UsuariosAPI.Services;
+
+namespace UsuariosAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private UsuarioService _usuarioService;
+
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost("cadastro")]
+        public async Task<IActionResult> CadastraUsuario(CreateUsuarioDTO dto)
+        {
+
+            await _usuarioService.Cadastra(dto);
+            return Ok("Usuário cadastrado!");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginUsuarioDTO dto)
+        {
+            var token = await _usuarioService.Login(dto);
+            return Ok(token);
+        }
+    }
+}
